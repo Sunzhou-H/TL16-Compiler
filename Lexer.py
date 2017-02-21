5ed98e10b91947af07d4ae7799d5705aba659821
wereHuang <hsz9@outlook.com>
2017-02-21 13:35:52 -0600

    Lexer V0.99 Add illegal int error

--- Lexer.py
+++ Lexer.py
@@ -36,18 +36,23 @@ tl_dict = {'(': 'LP',
            'readint': 'READINT',
            }
 
-re_num = re.compile(r'^([1-9][0-9]*|0)$')  # todo 32 int
+re_num = re.compile(r'^([1-9][0-9]*|0)$')
 re_boollit = re.compile(r'^(false|true)$')
 re_ident = re.compile(r'^([a-z_A-Z][a-zA-Z0-9]*)$')
 re_opr2 = re.compile(r'(:=|!=|<=|>=)')
 re_opr1 = re.compile(r'(\(|\)|;|\*|\+|-|=|<|>)')
+FLAG_ILLEGAL_INT = 0
 
 
 def match_token(key, f):
+    global FLAG_ILLEGAL_INT
     token = tl_dict.get(key)
     if token:
         pass
     elif re_num.match(key):
+        if int(key) > 2147483647:
+            FLAG_ILLEGAL_INT = 1
+            return False
         token = 'num(' + key + ')'
     elif re_boollit.match(key):
         token = 'boollit(' + key + ')'
@@ -67,4 +72,8 @@ with open(tl_file, 'r') as f_tl, open(tok_file, 'w') as f_tok:
                 if not match_token(temp_key2, f_tok):
                     for temp_key3 in re.sub(re_opr1, r' \1 ', temp_key2).split():
                         if not match_token(temp_key3, f_tok):
-                            print('SCANNER ERROR due to \"'+temp_key3+'\"')
+                            if FLAG_ILLEGAL_INT:
+                                print('SCANNER ERROR due to illegal integer \"'+temp_key3+'\"')
+                                FLAG_ILLEGAL_INT = 0
+                            else:
+                                print('SCANNER ERROR due to \"'+temp_key3+'\"')
