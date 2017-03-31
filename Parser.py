66734ef6911ac83d228daf5cf16654d4d8469eae
wereHuang <hsz9@outlook.com>
2017-03-31 17:46:59 -0500

    Parser: parsing function is done.

--- Parser.py
+++ Parser.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+current_tok = ''
+
+
+class ASTNode(object):
+    pass
+
+
 def parser(tok_file, ast_file):
     global current_tok
     with open(tok_file, 'r') as f_tok, open(ast_file, 'w') as f_ast:
@@ -9,7 +16,7 @@ def parser(tok_file, ast_file):
 
             def token(tok):
                 global current_tok
-                if tok == current_tok:
+                if tok in current_tok:
                     print(current_tok)
                     current_tok = next(source_toks)
                     return True
@@ -18,56 +25,44 @@ def parser(tok_file, ast_file):
 
             def epsilon():
                 return True
-            #todo match ident() to ident
 
             def program():
-                global current_tok
                 if current_tok == 'PROGRAM':
-                    return token('PROGRAM') and declaration() and token('BEGIN') and statementSequence() and token('END')
+                    return token('PROGRAM') and declaration() and token('BEGIN') and statement_sequence() and \
+                           token('END')
                 else:
                     return False
 
             def declaration():
-                global current_tok
                 if current_tok == 'VAR':
-                    return token('VAR') and token('ident') and token('AS') and type() and token('SC') and declaration()
+                    return token('VAR') and token('ident(') and token('AS') and type_tl() and token('SC') and \
+                           declaration()
                 elif current_tok == 'BEGIN':
                     return epsilon()
                 else:
                     return False
 
-            def statementSequence():
-                global current_tok
-                if current_tok == 'ident':
-                    return statement() and token('SC') and statementSequence()
-                elif current_tok == 'IF':
-                    return statement() and token('SC') and statementSequence()
-                elif current_tok == 'WHILE':
-                    return statement() and token('SC') and statementSequence()
-                elif current_tok == 'WRITEINT':
-                    return statement() and token('SC') and statementSequence()
-                elif current_tok == 'END':
-                    return epsilon()
-                elif current_tok == 'ELSE':
+            def statement_sequence():
+                if ('ident('in current_tok) or (current_tok in ('IF', 'WHILE', 'WRITEINT')):
+                    return statement() and token('SC') and statement_sequence()
+                elif current_tok in ('END', 'ELSE'):
                     return epsilon()
                 else:
                     return False
 
-            def  statement():
-                global current_tok
-                if current_tok == 'ident':
+            def statement():
+                if 'ident('in current_tok:
                     return assignment()
                 elif current_tok == 'IF':
-                    return ifStatement()
+                    return if_statement()
                 elif current_tok == 'WHILE':
-                    return whileStatement()
+                    return while_statement()
                 elif current_tok == 'WRITEINT':
-                    return writeInt()
+                    return writeint()
                 else:
                     return False
 
-            def type():
-                global current_tok
+            def type_tl():
                 if current_tok == 'INT':
                     return token('INT')
                 elif current_tok == 'BOOL':
@@ -76,154 +71,96 @@ def parser(tok_file, ast_file):
                     return False
 
             def assignment():
-                global current_tok
-                if current_tok == 'ident':
-                    return token('ident') and token('ASGN') and assignmentModified()
+                if 'ident('in current_tok:
+                    return token('ident(') and token('ASGN') and assignment_modified()
                 else:
                     return False
 
-            def ifStatement():
-                global current_tok
+            def if_statement():
                 if current_tok == 'IF':
-                    return token('IF') and expression() and token('THEN') and statementSequence() and elseClause() and token('END')
+                    return token('IF') and expression() and token('THEN') and statement_sequence() and else_clause() \
+                           and token('END')
                 else:
                     return False
 
-            def whileStatement():
-                global current_tok
+            def while_statement():
                 if current_tok == 'WHILE':
-                    return token('WHILE') and expression() and token('DO') and statementSequence() and token('END')
+                    return token('WHILE') and expression() and token('DO') and statement_sequence() and token('END')
                 else:
                     return False
 
-            def writeInt():
-                global current_tok
+            def writeint():
                 if current_tok == 'WRITEINT':
                     return token('WRITEINT') and expression()
                 else:
                     return False
 
-            def assignmentModified():
-                global current_tok
-                if current_tok == 'ident':
+            def assignment_modified():
+                if any(tok in current_tok for tok in ('ident(', 'num(', 'boollit(', 'LP')):
                     return expression()
                 elif current_tok == 'READINT':
                     return token('READINT')
-                elif current_tok == 'num':
-                    return expression()
-                elif current_tok == 'boollit':
-                    return expression()
-                elif current_tok == 'LP':
-                    return expression()
                 else:
                     return False
 
             def expression():
-                global current_tok
-                if current_tok == 'ident':
-                    return simpleExpression() and comp()
-                elif current_tok == 'num':
-                    return simpleExpression() and comp()
-                elif current_tok == 'boollit':
-                    return simpleExpression() and comp()
-                elif current_tok == 'LP':
-                    return simpleExpression() and comp()
+                if any(tok in current_tok for tok in ('ident(', 'num(', 'boollit(', 'LP')):
+                    return simple_expression() and comp()
                 else:
                     return False
 
-            def elseClause():
-                global current_tok
+            def else_clause():
                 if current_tok == 'ELSE':
-                    return token('ELSE') and statementSequence()
+                    return token('ELSE') and statement_sequence()
                 elif current_tok == 'END':
                     return epsilon()
                 else:
                     return False
 
-            def simpleExpression():
-                global current_tok
-                if current_tok == 'ident':
-                    return term() and add()
-                elif current_tok == 'num':
-                    return term() and add()
-                elif current_tok == 'boollit':
-                    return term() and add()
-                elif current_tok == 'LP':
+            def simple_expression():
+                if any(tok in current_tok for tok in ('ident(', 'num(', 'boollit(', 'LP')):
                     return term() and add()
                 else:
                     return False
 
             def comp():
-                global current_tok
-                if current_tok == 'COMPARE':
-                    return token('COMPARE') and expression()
-                elif current_tok == 'SC':
-                    return epsilon()
-                elif current_tok == 'THEN':
-                    return epsilon()
-                elif current_tok == 'DO':
+                if 'COMPARE(' in current_tok:
+                    return token('COMPARE(') and expression()
+                elif current_tok in ('SC', 'THEN', 'DO'):
                     return epsilon()
                 else:
                     return False
 
             def term():
-                global current_tok
-                if current_tok == 'ident':
-                    return factor() and multi()
-                elif current_tok == 'num':
-                    return factor() and multi()
-                elif current_tok == 'boollit':
-                    return factor() and multi()
-                elif current_tok == 'LP':
+                if any(tok in current_tok for tok in ('ident(', 'num(', 'boollit(', 'LP')):
                     return factor() and multi()
                 else:
                     return False
 
             def add():
-                global current_tok
-                if current_tok == 'ADDITIVE':
-                    return token('ADDITIVE') and simpleExpression()
-                elif current_tok == 'SC':
-                    return epsilon()
-                elif current_tok == 'THEN':
-                    return epsilon()
-                elif current_tok == 'DO':
-                    return epsilon()
-                elif current_tok == 'COMPARE':
-                    return epsilon()
-                elif current_tok == 'RP':
+                if 'ADDITIVE(' in current_tok:
+                    return token('ADDITIVE(') and simple_expression()
+                elif (current_tok in ('SC', 'THEN', 'DO', 'RP')) or ('COMPARE(' in current_tok):
                     return epsilon()
                 else:
                     return False
 
             def factor():
-                global current_tok
-                if current_tok == 'ident':
-                    return token('ident')
-                elif current_tok == 'num':
-                    return token('num')
-                elif current_tok == 'boollit':
-                    return token('boollit')
+                if 'ident(' in current_tok:
+                    return token('ident(')
+                elif 'num(' in current_tok:
+                    return token('num(')
+                elif 'boollit(' in current_tok:
+                    return token('boollit(')
                 elif current_tok == 'LP':
-                    return token('LP') and simpleExpression() and token('RP')
+                    return token('LP') and simple_expression() and token('RP')
                 else:
                     return False
 
             def multi():
-                global current_tok
-                if current_tok == 'MULTIPLICATIVE':
-                    return token('MULTIPLICATIVE') and term()
-                elif current_tok == 'ADDITIVE':
-                    return epsilon()
-                elif current_tok == 'COMPARE':
-                    return epsilon()
-                elif current_tok == 'SC':
-                    return epsilon()
-                elif current_tok == 'THEN':
-                    return epsilon()
-                elif current_tok == 'DO':
-                    return epsilon()
-                elif current_tok == 'RP':
+                if 'MULTIPLICATIVE(' in current_tok:
+                    return token('MULTIPLICATIVE(') and term()
+                elif any(tok in current_tok for tok in ('ADDITIVE(', 'COMPARE(', 'SC', 'THEN', 'DO', 'RP')):
                     return epsilon()
                 else:
                     return False
