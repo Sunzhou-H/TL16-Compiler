af7df410f020f6a0e1c6dfc93226f5a3872053c4
wereHuang <hsz9@outlook.com>
2017-04-30 22:25:08 -0500

    Code Generator 0.2.0 output MIPS without CFG

--- CodeGenerator.py
+++ CodeGenerator.py
@@ -3,10 +3,12 @@ import AST
 
 
 class CodeVisitor(AST.DummyVisitor):
-    def __init__(self, symbol_table, f_cfg):
+    def __init__(self, symbol_table, f_cfg, f_s):
         self.symbol_table = symbol_table
-        self.f_cfg = f_cfg
         self.stream = []
+        self.cfg_edges = []
+        self.f_cfg = f_cfg
+        self.f_s = f_s
 
     def visit_Program(self, node):
         if node.decl_list.decls:
@@ -40,7 +42,6 @@ class CodeVisitor(AST.DummyVisitor):
 
     def visit_Readint(self, node):
         r1 = Register.next()
-        # todo MIPS doesn't have readInt operator
         self.emit(Instruction('readInt', r1))
         node.reg = r1
 
@@ -72,7 +73,6 @@ class CodeVisitor(AST.DummyVisitor):
     def visit_Writeint(self, node):
         self.visit(node.expr)
         r1 = node.expr.reg
-        # todo MIPS doesn't have writeInt operator
         self.emit(Instruction('writeInt', r1))
 
     def visit_Comp(self, node):
@@ -122,8 +122,7 @@ class CodeVisitor(AST.DummyVisitor):
         elif node.op == 'div':
             self.emit(Instruction('div', r3, r1, r2))
         elif node.op == 'mod"':
-            # todo MIPS doesn't have mod operator
-            self.emit(Instruction('mod', r3, r1, r2))
+            self.emit(Instruction('remu', r3, r1, r2))
         else:
             raise CodeError
         node.reg = r3
@@ -139,6 +138,7 @@ class CodeVisitor(AST.DummyVisitor):
     def visit_Num(self, node):
         r1 = Register.next()
         self.emit(Instruction('li', r1, int(node.name)))
+        node.reg = r1
 
     def visit_Boollit(self, node):
         r1 = Register.next()
@@ -148,9 +148,53 @@ class CodeVisitor(AST.DummyVisitor):
             self.emit(Instruction('li', r1, 1))
         else:
             raise CodeError
+        node.reg = r1
 
     def emit(self, line):
         self.stream.append(line)
+
+    def output_mips(self):
+        self.f_s.write('\t.data\nnewline:\t.asciiz "\\n"\n\t.text\n\t.globl main\nmain:\n')
+        self.f_s.write('\tli $fp, 0x7ffffffc\n')
+        for ins in self.stream:
+            self.f_s.write(ins.output()+'\n')
+
+    def output_cfg(self):
+        for ins in self.stream:
+            print(ins.name, ins.block)
+        for edge in self.cfg_edges:
+            print(edge)
+
+    def create_cfg(self):
+        if isinstance(self.stream[0], Label):
+            block_index = 0
+        else:
+            block_index = 1
+        for ins in self.stream:
+            if isinstance(ins, Label):
+                block_index += 1
+                ins.block = block_index
+            elif ins.name == 'beqz':
+                ins.block = block_index
+                block_index += 1
+            else:
+                ins.block = block_index
+        self.cfg_edges = []
+        pre_ins = Instruction('temp')
+        pre_ins.block = 1
+        for ins in self.stream:
+            if ins.name == 'j' and ins.args[0]:
+                self.cfg_edges.append((ins.block, ins.args[0].block))
+            elif ins.name == 'beqz' and ins.args[1]:
+                self.cfg_edges.append((ins.block, ins.args[1].block))
+            if pre_ins.block != ins.block:
+                if pre_ins.name != 'j':
+                    self.cfg_edges.append((pre_ins.block, ins.block))
+            pre_ins = ins
+
+    def optimization(self):
+        self.emit(Instruction('exit'))
+        self.create_cfg()
 
 
 class Register(object):
@@ -158,6 +202,8 @@ class Register(object):
 
     def __init__(self, name):
         self.name = name
+        self.reg = name
+        #   todo reg
 
     @classmethod
     def next(cls):
@@ -166,22 +212,57 @@ class Register(object):
         return current
 
 
-class Label(object):
-    label = 0
-
-    def __init__(self, name):
-        self.name = name
-
-    @classmethod
-    def next(cls):
-        cls.label += 1
-        return Label('L'+str(cls.label)+':')
-
-
 class Instruction(object):
     def __init__(self, name, *args):
         self.name = name
         self.args = args
+        self.block = -1
+
+    def output(self):
+        if self.name == 'readInt':
+            ins = '\tli $v0, 5' + '\n\tsyscall\n' + '\tmove '
+            if self.args:
+                ins = ins + self.args[0].reg + ', $v0'
+            else:
+                raise CodeError
+            return ins
+        elif self.name == 'writeInt':
+            ins = '\tli $v0, 1'+'\n'+'\tmove $a0, '
+            if self.args:
+                ins = ins + self.args[0].reg + '\n\tsyscall'
+            else:
+                raise CodeError
+            return ins
+        elif self.name == 'exit':
+            ins = '\tli $v0, 10\n\tsyscall'
+            return ins
+        else:
+            ins = '\t' + self.name
+            for arg in self.args:
+                if isinstance(arg, Register):
+                    ins = ins + ' ' + str(arg.reg) + ','
+                elif isinstance(arg, Label):
+                    ins = ins + ' ' + str(arg.name)
+                else:
+                    ins = ins + ' ' + str(arg)
+            if ins.endswith(','):
+                ins = ins[:-1]
+            return ins
+
+
+class Label(Instruction):
+    label = 0
+
+    def __init__(self, name, *args):
+        super().__init__(name, args)
+
+    @classmethod
+    def next(cls):
+        cls.label += 1
+        return Label('L'+str(cls.label))
+
+    def output(self):
+        return self.name+':'
 
 
 class CodeError(Exception):
@@ -192,12 +273,14 @@ class InitError(CodeError):
     pass
 
 
-def code_generator(ast_tree, symbol_table, cfg_file):
-    with open(cfg_file, 'w') as f_cfg:
-        c_visitor = CodeVisitor(symbol_table, f_cfg)
+def code_generator(ast_tree, symbol_table, cfg_file, s_file):
+    with open(cfg_file, 'w') as f_cfg, open(s_file, 'w') as f_s:
+        c_visitor = CodeVisitor(symbol_table, f_cfg, f_s)
         try:
             c_visitor.visit(ast_tree)
-            print(1)
+            c_visitor.optimization()
+            c_visitor.output_cfg()
+            c_visitor.output_mips()
         except InitError:
             return False
         except CodeError:
--- Compiler.py
+++ Compiler.py
@@ -9,10 +9,11 @@ file_name = tl_file.split('.')[0]
 tok_file = file_name + '.tok'
 ast_file = file_name + '.ast.dot'
 cfg_file = file_name + '.3A.cfg.dot'
+s_file = file_name + '.s'
 
 if Lexer.lexer(tl_file, tok_file):
     temp = Parser.parser(tok_file, ast_file)
     if temp:
         ast_tree = temp[0]
         symbol_table = temp[1]
-        CodeGenerator.code_generator(ast_tree, symbol_table, cfg_file)
+        CodeGenerator.code_generator(ast_tree, symbol_table, cfg_file, s_file)
